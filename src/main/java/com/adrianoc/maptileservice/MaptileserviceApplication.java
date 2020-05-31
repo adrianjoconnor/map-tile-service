e7d7787629477663a386220a653aa05159d0e6e5
adrianoconnor <adrian@integrated-digital.com>
2020-05-31 18:06:31 +0100

    Initial commit.

---
+++ src/main/java/com/adrianoc/maptileservice/MaptileserviceApplication.java
@@ -0,0 +1,13 @@
+package com.adrianoc.maptileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaptileserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaptileserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adrianoc/maptileservice/MaptileserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrianoc.maptileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaptileserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
